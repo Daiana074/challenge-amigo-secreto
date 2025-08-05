8ef7cacd19e360e632dd3eaa1a2080ecc6e26735
Daiana <daii.camacho31@gmail.com>
2025-08-05 11:55:41 -0300

    Implementa la funcion agregarAmigos

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//crea un array para almacenar nombres
+let amigos = [];
+
+//implementa una funcion para agregar amigos
+function agregarAmigo(){
+    let amigo = document.getElementById('amigo').value;
+    if (amigo == ''){
+        alert('Por favor inserte un nombre');
+    }else{
+        amigos.push(amigo);
+        console.log(amigos);
+    }
+    limpiarCaja();
+}
+
+
+function limpiarCaja(){
+    let valorCaja = document.getElementById('amigo').value = '';
+    
+}
